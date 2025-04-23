56ac50a21f49bba4bf91ce2155e8f57a8ba1c609
Muhammad Faiz <muhammadfazifarid921@gmail.com>
2025-04-23 14:29:51 +0500

    fixed some bugs in favorites List

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -6,19 +6,22 @@ import MovieCard from "../components/MovieCard";
 
 function Favorites(){
     const { favorites } = useMovieContext()
-    if (favorites) {
-        return <div className="favorites">
+    if (favorites && favorites.length > 0 ) {
+        return (<div className="favorites">
             <h2>Your Favorites</h2>
         <div className="favorites-list">
             {favorites.map((movie => (<MovieCard key={movie.id} movie={movie} />
             )))}
         </div>
-        </div>
+    </div>
+        )
     }
-    return <div className="favorites-empty">
+    return (<div className="favorites-empty">
         <h2>No Favorite Movies Yet</h2>
         <p>Start adding movies to your favorites and they will appear here</p>
     </div>
+    )
+    
 }
 
 export default Favorites
