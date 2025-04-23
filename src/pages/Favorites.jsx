9b5e4b47f39a2b0af7fe1afb1923d1fe5bd91ea4
Muhammad Faiz <muhammadfazifarid921@gmail.com>
2025-04-23 12:57:09 +0500

    initial commit

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,24 @@
+import "../css/Favorites.css";
+import "../css/MovieCard.css";
+import "../css/Navbar.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+
+function Favorites(){
+    const { favorites } = useMovieContext()
+    if (favorites) {
+        return <div className="favorites">
+            <h2>Your Favorites</h2>
+        <div className="favorites-list">
+            {favorites.map((movie => (<MovieCard key={movie.id} movie={movie} />
+            )))}
+        </div>
+        </div>
+    }
+    return <div className="favorites-empty">
+        <h2>No Favorite Movies Yet</h2>
+        <p>Start adding movies to your favorites and they will appear here</p>
+    </div>
+}
+
+export default Favorites
